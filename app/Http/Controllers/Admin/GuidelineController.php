4eb718d8891cadccbd769fec8f794259e1880f35
mostafa yehia <96825436+mostafayehia2002@users.noreply.github.com>
2024-02-18 22:46:33 +0200

    Update GuidelineController.php

--- app/Http/Controllers/Admin/GuidelineController.php
+++ app/Http/Controllers/Admin/GuidelineController.php
@@ -15,10 +15,7 @@ class GuidelineController extends Controller
     {
         $this->middleware('auth:admin');
     }
-    public function __construct()
-    {
-        $this->middleware('auth:admin');
-    }
+   
     /**
      * Display a listing of the resource.
      */
