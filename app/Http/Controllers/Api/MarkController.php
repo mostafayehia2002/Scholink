dc4efcef36569f3f09ae2e3e79d75e9f075f1a48
teefa <gad993813@gmail.com>
2024-03-09 23:50:55 +0200

    finish table&marks in parent

--- app/Http/Controllers/Api/MarkController.php
+++ app/Http/Controllers/Api/MarkController.php
@@ -13,29 +13,61 @@ use Illuminate\Support\Facades\Validator;
 
 class MarkController extends Controller
 {
-use GeneralResponse;
-public function getStudentMarks(Request $request)
-{
-    try {
-        $validate = Validator::make(
-            $request->all(),
-            [
-               'level_id'=>'required|exists:levels,id',
-                'term'=>'required|in:first,second'
-            ]);
-        if ($validate->fails()) {
-            return $this->error(422,$validate->errors());
+    use GeneralResponse;
+    public function getStudentMarks(Request $request)
+    {
+        try {
+            $validate = Validator::make(
+                $request->all(),
+                [
+                    'level_id' => 'required|exists:levels,id',
+                    'term' => 'required|in:first,second',
+                ]
+            );
+            if ($validate->fails()) {
+                return $this->error(422, $validate->errors());
+            }
+            $student = auth('student')->user();
+            $marks = Mark::with('subject')
+                ->where('student_id', $student->id)
+                ->where('level_id', $request->level_id)
+                ->where('term', $request->term)
+                ->get();
+            if (count($marks) > 0) {
+                return $this->data(200, 'marks', MarksResource::collection($marks));
+            }
+            return $this->error(404, trans('response.Data_Not_Found'));
+        } catch (\Exception $e) {
+            return $this->error(500, $e->getMessage());
         }
-          $student=auth('student')->user();
-         $marks=Mark::with('subject')
-             ->where('student_id',$student->id)
-             ->where('level_id',$request->level_id)
-             ->where('term',$request->term)
-             ->get();
-        return $this->data(200,'marks',MarksResource::collection($marks));
-    }catch (\Exception $e){
-        return $this->error(500,$e->getMessage());
     }
 
-}
+    public function getMarks(Request $request)
+    {
+        try {
+            $validate = Validator::make(
+                $request->all(),
+                [
+                    'level_id' => 'required|exists:levels,id',
+                    'term' => 'required|in:first,second',
+                    'student_id' => 'required|exists:students,id',
+                ]
+            );
+            if ($validate->fails()) {
+                return $this->error(422, $validate->errors());
+            }
+            $student = Student::find($request->student_id);
+            $marks = Mark::with('subject')
+                ->where('student_id', $student->id)
+                ->where('level_id', $request->level_id)
+                ->where('term', $request->term)
+                ->get();
+            if (count($marks) > 0) {
+                return $this->data(200, 'marks', MarksResource::collection($marks));
+            }
+            return $this->error(404, trans('response.Data_Not_Found'));
+        } catch (\Exception $e) {
+            return $this->error(500, $e->getMessage());
+        }
+    }
 }
